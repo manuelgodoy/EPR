1da1db4a71b7a2f0970027489e603b2ae283e46b
manuelgodoy <manuelgm@gmail.com>
2016-11-02 17:15:35 -0500

    Changes to be committed: 	new file:   .ipynb_checkpoints/Untitled-checkpoint.ipynb 	new file:   Untitled.ipynb 	modified:   epr_amplitude.py 	new file:   epr_signal.py 	new file:   epr_sweep.py 	new file:   instrument_control.py 	modified:   pandas_magnet_sweep.py

--- Res403_Varactor070/epr_signal.py
+++ Res403_Varactor070/epr_signal.py
@@ -7,9 +7,24 @@ class EPR(object):
     def __init__(self, signal):
         self.signal = np.array(signal)
         self.time = datetime.datetime.now().strftime("%B %d %Y %H:%M:%S")
-        # self.maxima = None
-        # self.minima = None
-        # self.amplitude = None
+        # indices_max = argrelextrema(self.signal, np.greater)
+        # self.maxima = self.signal[indices_max[0]].max()
+        self.maxima = self.find_maxima(self.signal)
+        # indices_min = argrelextrema(self.signal, np.less)
+        # self.minima = self.signal[indices_min[0]].min()
+        self.minima = self.find_minima(self.signal)
+        self.amplitude = self.maxima - self.minima
+
+    # @classmethod
+    def find_maxima(self, signal):
+        indices_max = argrelextrema(signal, np.greater)
+        maxima = self.signal[indices_max[0]].max()
+        return maxima
+
+    def find_minima(self, signal):
+        indices_min = argrelextrema(signal, np.less)
+        minima = self.signal[indices_min[0]].min()
+        return minima
 
     def set_maxima(self):
         # for local maxima
@@ -23,17 +38,17 @@ class EPR(object):
         self.minima = self.signal[indices_min[0]].min()
         return self.minima
 
-    def set_amplitude(self):
+    # def set_voltage_vector(self, voltage):
+    #     self.voltage = np.array(voltage)
+    #     return
+    #
+    # def set_bfield_vector(self, bfield):
+    #     self.bfield = np.array(bfield)
+    #     return
+
+    def amplitude(self):
         self.amplitude = self.maxima - self.minima
         return self.amplitude
-
-    def set_voltage_vector(self, voltage):
-        self.voltage = np.array(voltage)
-        return
-
-    def set_bfield_vector(self, bfield):
-        self.bfield = np.array(bfield)
-        return
 
     def peaks(self):
         """signal must be a np array"""
@@ -55,32 +70,14 @@ class EPR(object):
         return (max_ind+min_ind)/2
         # return None
 
-    def noise(self):
+    def noise(self, sample_size = 5, from_beginning = True):
         """This functions takes 5 measurements away from the EPR signal and
         measures their std dev. A phase difference at the mixer may skey this value
         """
-        return self.signal[:5].std()
+        if from_beginning:
+            return self.signal[:sample_size].std()
+        else:
+            return self.signal[-sample_size:].std()
 
-def peaks(signal):
-    """signal must be a np array"""
-
-    # for local maxima
-    indices_max = argrelextrema(signal, np.greater)
-
-    # for local minima
-    indices_min = argrelextrema(signal, np.less)
-
-    maxima = signal[indices_max[0]].max()
-    minima = signal[indices_min[0]].min()
-
-    return maxima, minima
-
-def amplitude(signal):
-    maxima, minima = peaks(signal)
-    return maxima - minima
-
-def mid_point_index(signal):
-    maxima, minima = peaks(signal)
-    max_ind = np.where(signal == maxima)[0][0]
-    min_ind = np.where(signal == minima)[0][0]
-    return abs((max_ind+min_ind)/2)
+    def to_db(self):
+        return 10*np.log10(self.signal*self.signal/0.050)
